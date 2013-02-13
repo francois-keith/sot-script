d5907ada431e632ce0288e679469117e1de04a3d
Francois Keith <keith@lirmm.fr>
2013-02-14 00:47:37 +0100

    Separate scripts for ubuntu 12-10 and 12-04.

--- unix-dependency_12_10.sh
+++ unix-dependency_12_10.sh
@@ -7,7 +7,7 @@ sudo apt-get install cmake g++ doxygen gfortran
 # lapack dependencies
 sudo apt-get install liblapack3gf libblas3GF liblapack-dev libblas-dev
 # boost dependencies
-sudo apt-get install libboost-thread1.46-dev libboost-date-time1.46-dev libboost-filesystem1.46-dev libboost-program-options1.46-dev libboost-serialization1.46-dev libboost-signals1.46-dev libboost-system1.46-dev libboost-test1.46-dev
+sudo apt-get install libboost1.49-dev libboost-thread1.49-dev libboost-date-time1.49-dev libboost-filesystem1.49-dev libboost-program-options1.49-dev libboost-serialization1.49-dev libboost-signals1.49-dev libboost-system1.49-dev libboost-test1.49-dev
 #  boost-build 
 # pytohn 2.6 dependencies
 sudo apt-get install python2.7-dev python-numpy python-sphinx python-opengl python-setuptools python-yaml
