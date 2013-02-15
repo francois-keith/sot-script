ce5b2d7712ce308c7db3a9e15f3f97f798e71f1a
Francois Keith <keith@lirmm.fr>
2013-02-15 17:43:06 +0100

    Add the installation of eigen in the scripts.

--- unix-dependency_12_04.sh
+++ unix-dependency_12_04.sh
@@ -8,9 +8,10 @@ sudo apt-get install cmake g++ doxygen gfortran
 sudo apt-get install liblapack3gf libblas3GF liblapack-dev libblas-dev
 # boost dependencies
 sudo apt-get install libboost-thread1.46-dev libboost-date-time1.46-dev libboost-filesystem1.46-dev libboost-program-options1.46-dev libboost-serialization1.46-dev libboost-signals1.46-dev libboost-system1.46-dev libboost-test1.46-dev
-#  boost-build 
-# pytohn 2.6 dependencies
+# python 2.7 dependencies
 sudo apt-get install python2.7-dev python-numpy python-sphinx python-opengl python-setuptools python-yaml
+# eigen
+sudo apt-get install libeigen3-dev
 
 # download http://sourceforge.net/projects/simpleparse/files/simpleparse/2.1.1a2/
 # install it using 
